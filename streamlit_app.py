265e52899103c97063e208d161219db3d12324d8
hoanghao123code <21T1020347@husc.edu.vn>
2024-09-15 21:00:14 +0700

    update3

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,42 +13,9 @@ from PIL import Image
 # from rembg import remove
 from streamlit_drawable_canvas import st_canvas
 
-st.title('🎈 GrabCut App')
+st.title('🎈Hoang Hao GrabCut App')
 
 
-# st.write('Hello world!')
-
-#!/usr/bin/env python
-'''
-===============================================================================
-# Grabcut
-
-A simple program for interactively removing the background from an image using
-the grab cut algorithm and OpenCV.
-
-This code was derived from the Grab Cut example from the OpenCV project.
-
-## Operation
-
-At startup, two windows will appear, one for input and one for output.
-
-To start, in input window, draw a rectangle around the object using mouse right
-button.  For finer touch-ups, press any of the keys below and draw circles on
-the areas you want.  Finally, press 's' to save the result.
-
-## Keys
-  * 0 - Select areas of sure background
-  * 1 - Select areas of sure foreground
-  * 2 - Select areas of probable background
-  * 3 - Select areas of probable foreground
-  * n - Update the segmentation
-  * r - Reset the setup
-  * s - Save the result
-  * q - Quit
-===============================================================================
-'''
-
-# Python 2/3 compatibility
 
 
 
@@ -57,6 +24,7 @@ if __name__ == '__main__':
     image_upload = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
     
     if image_upload is not None:
+        st.write("Ảnh gốc")
         st.image(image_upload)
         drawing_mode = st.sidebar.selectbox("Drawing tool:", ("point", "freedraw", "line", "rect", "circle", "transform")
         )
@@ -88,6 +56,7 @@ if __name__ == '__main__':
             point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
             key="canvas",
         )
-        st.download_button(
-            "Download grabcut image", image_upload, "fixed.png", "image/png"
-        )
+        # st.image(canvas_result.image_data)
+        # st.download_button(
+        #     "Download grabcut image", canvas_result.image_data, "fixed.png", "image/png"
+        # )
