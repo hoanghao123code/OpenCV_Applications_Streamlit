3be00e445dcd84a20f52f5649653b23b06ebb247
hoanghao123code <21T1020347@husc.edu.vn>
2024-09-20 16:18:46 +0700

    update version rqm

---
+++ Welcome.py
@@ -0,0 +1,16 @@
+# Python 2/3 compatibility
+from __future__ import print_function
+
+import numpy as np
+import cv2 as cv
+import sys
+import streamlit as st
+import tempfile
+import os
+
+from io import BytesIO
+from PIL import Image
+# from rembg import remove
+from streamlit_drawable_canvas import st_canvas
+
+st.title('🎈Hoang Hao Applications')
