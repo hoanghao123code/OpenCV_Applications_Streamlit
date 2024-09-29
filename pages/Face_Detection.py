5573acccda81508661a5beb7d345a158de2583b9
hoanghao123code <21T1020347@husc.edu.vn>
2024-09-29 10:15:44 +0700

    up haar

--- pages/Face_Detection.py
+++ pages/Face_Detection.py
@@ -1,5 +1,6 @@
 from time import time
 
+import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 import os
@@ -29,58 +30,104 @@ def extract_feature_image(img, feature_type, feature_coord=None):
     )
     
 # Tập dataset 
-face = []
-non_face = []
+# Load image
+# face = []
+# non_face = []
 
 # dir_face = os.listdir('./images/faces_24x24/')
-# print(dir_face)
+# dir_non_face = os.listdir('./images/non_faces_24x24/')
 
+# for i in range(len(dir_face)):
+#     path_face =  './images/faces_24x24/' + dir_face[i]
+#     path_non = './images/non_faces_24x24/' + dir_non_face[i]
+    
+#     # path = 'D:\\OpenCV\\Grabcut\\Grabcut_Streamlit\\images\\faces_24x24\\' + dir_face[i]
+#     # print(path, 1, 0.5, 0.5, 23.5, 23.5)
+#     # print(path)
+#     image_face = cv.imread(path_face)
+#     image_face = cv.cvtColor(image_face, cv.COLOR_BGR2GRAY)
+#     face.append(image_face)
+    
+#     image_non = cv.imread(path_non)
+#     image_non = cv.cvtColor(image_non, cv.COLOR_BGR2GRAY)
+#     non_face.append(image_non)
+
+# face_dataset = [ ]
+# for i in range(len(face)):
+#     face_dataset.append(face[i])
+# for i in range(len(non_face)):
+#     face_dataset.append(non_face[i])
+# labels = np.array([1] * 440 + [0] * 400)
+# face_dataset = np.array(face_dataset)
+
+
+
+# print(labels)
 # To speed up the example, extract the two types of features only
 # Sử dụng 5 feature
 # feature_types = ['type-2-x', 'type-2-y', 'type-3-x', 'type-3-y', 'type-4']
 
 # # Compute the result
 # t_start = time()
-# X = [extract_feature_image(img, feature_types) for img in images]
+# X = [extract_feature_image(image, feature_types) for image in face_dataset]
 
 # # Với mỗi x trong X chứa khoảng 190k feature type (bộ lọc)
 # X = np.stack(X)
 # time_full_feature_comp = time() - t_start
 
+# print(time_full_feature_comp)
 # # Label images (100 faces and 100 non-faces)
-# y = np.array([1] * 100 + [0] * 100)
-
+# # y = np.array([1] * 100 + [0] * 100)
+# y = labels
 # # Chia train, test
 # X_train, X_test, y_train, y_test = train_test_split(
 #     X, y, train_size=150, random_state=0, stratify=y
 # )
 
+# print(len(X_train), len(y_train))
+
 
 # Importing OpenCV package 
 # import cv2 
-# import matplotlib.pyplot as plt
-# import streamlit as st
+import matplotlib.pyplot as plt
+import streamlit as st
+from PIL import Image
 
-# img = cv2.imread('D:\\OpenCV\\Grabcut\\Grabcut_Streamlit\\images\\images.png') 
-# # Converting image to grayscale 
-# gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+image_upload = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+# image_upload = cv.imread('./images/faces_24x24/s1_1.png') 
 
-# # Loading the required haar-cascade xml classifier file 
-# haar_cascade = cv2.CascadeClassifier('D:\\OpenCV\\WaterShed\\haarcascade_frontalface_default.xml') 
-# if haar_cascade.empty():
-#     print("Không thể mở tệp Haar Cascade. Vui lòng kiểm tra đường dẫn.")
-# else:
-#     print("Tệp Haar Cascade đã được mở thành công.")
-# # Applying the face detection method on the grayscale image 
-# faces_rect = haar_cascade.detectMultiScale(gray_img, 1.1, 9) 
+ 
+if image_upload is not None:
+# Converting image to grayscale
+    if not os.path.exists('images'):
+        os.makedirs('images')
+    image = Image.open(image_upload)
+    image.save('images/' + image_upload.name)
+    img = cv.imread('images/' + image_upload.name)
+    
+    if img is not None and len(img.shape) == 3:
+        gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY) 
 
+        # Loading the required haar-cascade xml classifier file 
+        # haar_cascade = cv.CascadeClassifier('./images/haarcascade_frontalface_default.xml') 
 
+        if os.path.exists('.\\images\\haarcascade_frontalface_default.xml'):
+            print(1)
+        else:
+            print(0)
 
-# # Iterating through rectangles of detected faces 
-# for (x, y, w, h) in faces_rect: 
-# 	cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2) 
+        # Applying the face detection method on the grayscale image 
+        # face_rect = 0
+        # # if gray_img is not None:
+        # faces_rect = haar_cascade.detectMultiScale(gray_img, 1.1, 9) 
 
-# plt.imshow(img) 
-# plt.axis('off')
+        # # print(len(faces_rect))
+
+        # # Iterating through rectangles of detected faces 
+        # print(face_rect)
+        # for (x, y, w, h) in faces_rect: 
+        #     cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2) 
+
+        # st.image(img)
 
 
