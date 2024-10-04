1b7e8bd1ff8a323cbaa1a3e14cfa11d8a0763fe8
hoanghao123code <21T1020347@husc.edu.vn>
2024-10-04 21:39:29 +0700

    face detection

--- pages/Face_Detection.py
+++ pages/Face_Detection.py
@@ -26,7 +26,7 @@ from skimage.feature import haar_like_feature
 from skimage.feature import haar_like_feature_coord
 from skimage.feature import draw_haar_like_feature
 
-cascade_file = 'D:\OpenCV\Grabcut\Grabcut_Streamlit\images\Face_detect\cascade.xml'
+cascade_file = 'D:\\OpenCV\\Grabcut\\Grabcut_Streamlit\\images\\Face_detect\\cascade.xml'
 
 tree = ET.parse(cascade_file)
 
@@ -79,8 +79,8 @@ face_dataset = np.array(face_dataset)
 
 # Trích xuất đặc trưng ảnh
 def extract_feature_image(img):
-    image_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    image = cv.resize(image_gray, (24, 24))
+    image = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    image = cv.resize(image, (24, 24))
     ii = cv.integral(image)
     val = 0
     for rect in haar_features:
@@ -89,13 +89,17 @@ def extract_feature_image(img):
     return val
 
 
+# X_train = []
+
 # def extract_image_dataset():
 #     for i in range(len(face_dataset)):
 #         value = extract_feature_image(face_dataset[i])
 #         X_train.append(value)
 # extract_image_dataset()
-# # print(X_train)
+# print(X_train)
 # y_train = labels
+
+# X_train = np.array(X_train)
 
 # pickle_file = 'D:\OpenCV\Grabcut\Grabcut_Streamlit\images\Train_test\X_train.pkl'
 # with open(pickle_file, 'wb') as file:
@@ -105,65 +109,79 @@ def extract_feature_image(img):
 # with open(pickle_file_y, 'wb') as file:
 #     pickle.dump(y_train, file)
 
-# model = KNeighborsClassifier(n_neighbors = 5)
-# model.fit(X_train, y_train)
+
 X_train = []
 y_train = []
-# with open('./images/Train_test/X_train.pkl/', 'rb') as file:
-#     X_train = pickle.load(file)
+with open('./images/Train_test/X_train.pkl', 'rb') as file:
+    X_train = pickle.load(file)
 
-# with open('./images/Train_test/y_train.pkl/', 'rb') as file:
-#     y_train = pickle.load(file)
-if os.path.exists('./images/faces_24x24'):
-    print(1)
-else:
-    print(0)
-# def detect_face_Sub_window(image):
-#     sz = 24
-#     step = 2
-#     lst_rect = []
-#     for x in range(0, image.shape[0] - sz, step):
-#         for y in range(0, image.shape[1] - sz, step):
-#             sub_window = image[y + sz, x + sz]
-#             feature_sub = extract_feature_image(sub_window)
-#             predictions = model.predict(features)
-#             if predictions[0] == 1:
-#                 lst_rect.append(x, y, sz, sz)
-#     return lst_rect
+with open('./images/Train_test/y_train.pkl', 'rb') as file:
+    y_train = pickle.load(file)
+    
+X_train = X_train.reshape(-1, 1)
+y_train = y_train.reshape(-1, 1)
+print(X_train.shape, y_train.shape)
+model = KNeighborsClassifier(n_neighbors = 10)
+model.fit(X_train, y_train)
+
+def detect_face_Sub_window(image):
+    sz = 50
+    step = image.shape[0] // 20
+    lst_rect = []
+    for x in range(0, image.shape[1] - sz, step):
+        for y in range(0, image.shape[0] - sz, step):
+            sub_window = image[y : y + sz, x : x + sz]
+            feature_sub = extract_feature_image(sub_window)
+            predictions = model.predict(np.array([[feature_sub]]))
+            if predictions[0] == 1:
+                lst_rect.append((x, y, sz, sz))
+    return lst_rect
     
 
+def IoU(box1, box2):
+    x1, y1, w1, h1 = box1
+    x2, y2, w2, h2 = box2
+    x_max = max(x1, x2)
+    x_min = min(x1 + w1, x2 + w2)
+    y_max = max(y1, y2)
+    y_min = min(y1 + h1, y2 + h2)
+    intersect = max(0, x2 - x1) * max(0, y2 - y1)
+    area_1 = w1 * h1
+    area_2 = w2 * h2
+    union_area = area_1 + area_2 - intersect
+    return intersect / union_area if union_area != 0 else 0 
+    
+# Non Maximum Suppression
 
-# Importing OpenCV package 
-# import cv2 
+def NMS(boxes, Iou_threshold):
+    choose_boxes = []
+    
+    boxes = sorted(boxes, key = lambda box : box[2] * box[3], reverse = True)
+    while boxes:
+        cur_box = boxes.pop(0)
+        choose_boxes.append(cur_box)
+        
+        boxes = [box for box in boxes if IoU(cur_box, box) < Iou_threshold] 
+    return choose_boxes
 
-# st.markdown("#### Chọn ảnh bạn cần phát hiện khuôn mặt")
-# image_upload = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+
+st.markdown("#### Chọn ảnh bạn cần phát hiện khuôn mặt")
+image_upload = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 
  
-# if image_upload is not None:
-#     if not os.path.exists('images'):
-#         os.makedirs('images')
-#     image = Image.open(image_upload)
-#     image.save('images/' + image_upload.name)
-#     img = cv.imread('images/' + image_upload.name)
-    
-#     if img is not None and len(img.shape) == 3:
-#         gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY) 
-
-#         # Loading the required haar-cascade xml classifier file 
-#         haar_cascade = cv.CascadeClassifier('D:\OpenCV\Grabcut\Grabcut_Streamlit\images\Face_detect\cascade.xml') 
-
-#         # Applying the face detection method on the grayscale image 
-#         faces_rect = haar_cascade.detectMultiScale(gray_img, 1.1, 9)
-#         # Iterating through rectangles of detected faces 
-        
-#         features = []
-#         for (x, y, w, h) in faces_rect:
-#             features.append([x, y, w, h])
-#             img = cv.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-#         # predictions = Knn.predict(features)
-#         # print(type(predictions))
-#         st.image(img, channels="BGR")
+if image_upload is not None:
+    if not os.path.exists('images'):
+        os.makedirs('images')
+    image = Image.open(image_upload)
+    image.save('images/' + image_upload.name)
+    img = cv.imread('images/' + image_upload.name)
+    img_copy = img.copy()
+    if img is not None and len(img.shape) == 3:
+        faces_rect = detect_face_Sub_window(img)
+        faces_rect = NMS(faces_rect, float(0.15))
+        for (x, y, w, h) in faces_rect:
+            img = cv.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        st.image(img, channels="BGR")
             
 
 
